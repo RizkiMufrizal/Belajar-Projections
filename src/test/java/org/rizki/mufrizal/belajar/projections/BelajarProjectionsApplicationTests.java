86fb4cccaa031308a6ad8b1b0589b799c9586da5
RizkiMufrizal <mufrizalrizki@gmail.com>
2023-11-04 12:30:20 +0700

    initiate project

---
+++ src/test/java/org/rizki/mufrizal/belajar/projections/BelajarProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.belajar.projections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarProjectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
